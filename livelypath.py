349aec539c04cfa2430237028fbe7874d806fda8
messinis <di.messinis@gmail.com>
2023-03-30 23:42:30 +0200

    Updated script

--- livelypath.py
+++ livelypath.py
@@ -1,4 +1,3 @@
-import requests
 import streamlit as st
 import googlemaps
 import polyline
@@ -7,8 +6,8 @@ from streamlit_folium import folium_static
 import geopy.distance
 
 import os
-API_KEY = os.environ.get("API_KEY")
 
+API_KEY = os.environ.get("API_KEY")
 gmaps = googlemaps.Client(key=API_KEY)
 
 st.set_page_config(layout="wide", page_title="Busy Path Finder")
@@ -38,33 +37,21 @@ def get_best_route(gmaps, origin, destination):
         destination=destination,
         mode="walking",
         waypoints=waypoints,
-        optimize_waypoints=True
+        optimize_waypoints=True,
     )
+
+    best_route = []
+    for leg in directions[0]['legs']:
+        for step in leg['steps']:
+            start = (step['start_location']['lat'], step['start_location']['lng'])
+            end = (step['end_location']['lat'], step['end_location']['lng'])
+            segment_length = geopy.distance.distance(start, end).m
+            if segment_length > 100:  # Only include segments longer than 100m
+                best_route.append(start)
+            best_route.append(end)
     
-    segments = directions[0]["legs"][0]["steps"]
-    path = []
-    for i in range(len(segments)):
-        start = segments[i]["start_location"]
-        end = segments[i]["end_location"]
-        segment_start = (start["lat"], start["lng"])
-        segment_end = (end["lat"], end["lng"])
-        if i == 0:
-            path.append(segment_start)
-        if i == len(segments) - 1:
-            path.append(segment_end)
-        distance = geopy.distance.distance(segment_start, end).m
-        if distance > 100:
-            num_intermediate_points = int(distance / 100) + 1
-            lat_diff = (end["lat"] - start["lat"]) / num_intermediate_points
-            lng_diff = (end["lng"] - start["lng"]) / num_intermediate_points
-            for j in range(num_intermediate_points):
-                intermediate_lat = start["lat"] + j * lat_diff
-                intermediate_lng = start["lng"] + j * lng_diff
-                path.append((intermediate_lat, intermediate_lng))
-        else:
-            path.append(segment_end)
-    
-    return path
+    # Return the list of coordinates making up the best route
+    return best_route
 
 # Input origin and destination
 with st.form("inputs"):
@@ -82,11 +69,16 @@ if submitted:
             origin_coords = origin_geocode[0]['geometry']['location']
             destination_coords = destination_geocode[0]['geometry']['location']
 
-            best_route = get_best_route(gmaps, origin_coords, destination_coords)
+            best_route_coords = get_best_route(gmaps, origin_coords, destination_coords)
 
             # Display the map with the best route
             m = folium.Map(location=[origin_coords["lat"], origin_coords["lng"]], zoom_start=14)
-            folium.PolyLine(best_route, color="blue", weight=2.5, opacity=1).add_to(m)
+
+            for i in range(len(best_route_coords)-1):
+                start = best_route_coords[i]
+                end = best_route_coords[i+1]
+                folium.PolyLine([start, end], color="blue", weight=2.5, opacity=1).add_to(m)
+                
             folium_static(m)
         else:
             st.error("Invalid addresses entered. Please try again.")
