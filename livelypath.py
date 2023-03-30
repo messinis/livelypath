653770dd3744c032bbf6a9078982f53ae03580d6
messinis <di.messinis@gmail.com>
2023-03-30 23:29:39 +0200

    Updated script

--- livelypath.py
+++ livelypath.py
@@ -13,62 +13,68 @@ gmaps = googlemaps.Client(key=API_KEY)
 st.set_page_config(layout="wide", page_title="Busy Path Finder")
 st.title("Busy Path Finder")
 
+import geopy.distance
 
 def get_best_route(gmaps, origin, destination):
     busy_places_types = ['cafe', 'bar', 'restaurant']
     busy_places_radius = 200  # Adjust the radius as needed
+    min_segment_length = 100  # Adjust the minimum segment length as needed
 
-    # Get the route between the origin and destination
+    waypoints = []
+    segment_start = origin
+    route_segments = []
+
+    for place_type in busy_places_types:
+        busy_places = gmaps.places_nearby(
+            location=origin,
+            radius=busy_places_radius,
+            type=place_type,
+        )
+
+        for place in busy_places['results']:
+            lat = place['geometry']['location']['lat']
+            lng = place['geometry']['location']['lng']
+            waypoint = f"{lat},{lng}"
+            waypoints.append(waypoint)
+
     directions = gmaps.directions(
         origin=origin,
         destination=destination,
         mode="walking",
+        waypoints=waypoints,
     )
 
-    # Extract the polyline representing the route
-    route_polyline = directions[0]["overview_polyline"]["points"]
-    route_points = polyline.decode(route_polyline)
+    # Split the route into segments based on the distance between waypoints
+    for i, leg in enumerate(directions[0]['legs']):
+        for j, step in enumerate(leg['steps']):
+            start = (step['start_location']['lat'], step['start_location']['lng'])
+            end = (step['end_location']['lat'], step['end_location']['lng'])
+            distance = geopy.distance.distance(segment_start, end).m
 
-    # Select waypoints based on nearby busy places
-    waypoints = []
-    for i in range(len(route_points) - 1):
-        segment_start = route_points[i]
-        segment_end = route_points[i + 1]
-        segment_length = geopy.distance.distance(segment_start, segment_end).m
+            if distance > min_segment_length:
+                route_segments.append((segment_start, start))
+                segment_start = start
 
-        for place_type in busy_places_types:
-            busy_places = gmaps.places_nearby(
-                location=segment_start,
-                radius=min(busy_places_radius, segment_length),
-                type=place_type,
-            )
+    route_segments.append((segment_start, destination_coords))
 
-            if not busy_places["results"]:
-                continue
+    # Calculate the route through the segments
+    route_waypoints = []
 
-            # Select the closest place along the segment
-            closest_place = min(
-                busy_places["results"],
-                key=lambda place: geopy.distance.distance(
-                    (place["geometry"]["location"]["lat"], place["geometry"]["location"]["lng"]),
-                    segment_start,
-                ).m,
-            )
+    for segment_start, segment_end in route_segments:
+        segment_directions = gmaps.directions(
+            origin=f"{segment_start[0]},{segment_start[1]}",
+            destination=f"{segment_end[0]},{segment_end[1]}",
+            mode="walking",
+            waypoints=waypoints,
+        )
 
-            lat = closest_place["geometry"]["location"]["lat"]
-            lng = closest_place["geometry"]["location"]["lng"]
-            waypoint = f"{lat},{lng}"
-            waypoints.append(waypoint)
+        for i, leg in enumerate(segment_directions[0]['legs']):
+            for j, step in enumerate(leg['steps']):
+                for point in polyline.decode(step['polyline']['points']):
+                    route_waypoints.append((point[0], point[1]))
 
-    # Get the best route with the selected waypoints
-    best_route = gmaps.directions(
-        origin=origin,
-        destination=destination,
-        mode="walking",
-        waypoints=waypoints,
-    )[0]
-
-    return best_route
+    # Return the route as a list of (latitude, longitude) tuples
+    return route_waypoints
 
 
 # def get_best_route(gmaps, origin, destination):
